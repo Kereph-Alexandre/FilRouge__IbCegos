923d30e69f1c4b475308c7f0d68c007a486b2eb6
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-17 21:01:07 +0100

    feat: Ajout de lien aux cartes :sparkles:

--- src/composants/AffichageCartes/AffichageCartesPrestation.jsx
+++ src/composants/AffichageCartes/AffichageCartesPrestation.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { CartePrestation } from "../cartePrestation/cartePrestation";
 import "./AffichageCartesPrestation.css";
 
@@ -5,7 +6,9 @@ export const AffichageCartePrestation = (props) => {
   return (
     <div className="affichagePrestations">
       {props.listePrestations.map((prestation, index) => (
-        <CartePrestation key={index} {...prestation} />
+        <Link to={`/FichePrestation/${props.id}`}>
+          <CartePrestation key={index} {...prestation} />
+        </Link>
       ))}
     </div>
   );
