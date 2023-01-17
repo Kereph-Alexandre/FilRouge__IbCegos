c9bdac60069a314c920d6c396dcc86370e34d9bd
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-17 23:30:33 +0100

    feat: Ajout et mise en forme des descriptions de prestation :lipstick:

--- src/composants/FichePrestation/FichePrestation.jsx
+++ src/composants/FichePrestation/FichePrestation.jsx
@@ -3,22 +3,51 @@ import "./FichePrestation.css";
 export const FichePrestation = (props) => {
   return (
     <main>
-      <section className="imagePrestation">
-        <h2>{props.titre}</h2>
-        <img src={props.img} alt={props.titre} />
-      </section>
-      <aside className="informationPrestation">
-        <p>
-          {props.prestataire}, {props.nombrePrestations} prestations jusqu'à
-          présent
-        </p>
-        {/* Note */}
-        <p>{props.tauxHoraire}€/h</p>
-        <p>{props.localisation}</p>
-        <p>{props.datePublication}</p>
-      </aside>
-      <article className="descriptionPrestation"></article>
-      <article className="secondairePrestation"></article>
+      <article className="presentationPrestation">
+        <section className="imagePrestation">
+          <h2>{props.titre}</h2>
+          <img src={props.img} alt={props.titre} />
+        </section>
+        <aside className="informationPrestation">
+          <h2>Information Principales</h2>
+
+          <div className="sectionInformation">
+            <span className="labelInformationPrincipale">Prestataire :</span>
+            <span className="information">{props.prestataire},</span>
+          </div>
+          <div className="sectionInformation">
+            {props.nombrePrestations} prestations jusqu'à présent
+          </div>
+          <div className="sectionInformation">
+            <span className="labelInformationPrincipale">Note Moyenne</span>
+            <span className="information">{props.note}/5</span>
+            {/* Note */}
+          </div>
+
+          <div className="sectionInformation">
+            <span className="labelInformationPrincipale">Taux Horaire : </span>
+            <span className="information">{props.tauxHoraire}€/h</span>
+          </div>
+
+          <div className="sectionInformation">
+            <span className="labelInformationPrincipale">Localisation : </span>
+            <span className="information">{props.localisation}</span>
+          </div>
+
+          <div className="sectionInformation">
+            <span className="labelInformationPrincipale">Publication :</span>
+            <span className="information">{props.datePublication}</span>
+          </div>
+        </aside>
+      </article>
+      <article className="descriptionPrestation">
+        <h2>Description de la Prestation</h2>
+        <p>{props.description}</p>
+      </article>
+      <article className="secondairePrestation">
+        <h2>Informations secondaires</h2>
+        <p>...</p>
+      </article>
     </main>
   );
 };
