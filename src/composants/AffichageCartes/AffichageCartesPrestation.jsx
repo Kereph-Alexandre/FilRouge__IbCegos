3d107dadf1ad3756aee7e9953100ca540553cfdb
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-17 21:15:02 +0100

    struc: prép à recevoir des données :construction:
    Ajout de UseState/useEffect pour recevoir un tableau de prestations à afficher. Il faudra encore ajouter des liens à chanque carte

--- src/composants/AffichageCartes/AffichageCartesPrestation.jsx
+++ src/composants/AffichageCartes/AffichageCartesPrestation.jsx
@@ -1,14 +1,23 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { CartePrestation } from "../cartePrestation/cartePrestation";
 import "./AffichageCartesPrestation.css";
 
-export const AffichageCartePrestation = (props) => {
+import prestations from "../../données/donnéesPrestations.json";
+
+export const AffichageCartePrestation = () => {
+  const [listePrestations, setListePrestation] = useState([]);
+
+  useEffect(() => {
+    setListePrestation(prestations);
+  }, []);
+
   return (
     <div className="affichagePrestations">
-      {props.listePrestations.map((prestation, index) => (
-        <Link to={`/FichePrestation/${props.id}`}>
-          <CartePrestation key={index} {...prestation} />
-        </Link>
+      {listePrestations.map((prestation) => (
+        // Rajouter <Link to={`/FichePrestation/${prestation.id}`}> une fois que les routes vers les pages d'accueils et de fiche Prestations seront faites
+        <CartePrestation key={prestation.id} {...prestation} />
+        // </Link>
       ))}
     </div>
   );
