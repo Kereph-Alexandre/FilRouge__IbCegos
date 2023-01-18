3ef8435981302eff94a91b432a1a9c726321388e
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-18 22:58:49 +0100

    feat: Ajout des paramètres id pour trouver la prestation :sparkles:

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,15 @@
 import { Accueil } from "../pages/Accueil";
 import { useRoutes } from "react-router-dom";
+import { FichePrestation } from "../composants/FichePrestation/FichePrestation";
 export const Router = () => {
   const routes = [
     {
       path: "/",
       element: <Accueil />,
+    },
+    {
+      path: "/FichePrestation/:id",
+      element: <FichePrestation />,
     },
   ];
 
