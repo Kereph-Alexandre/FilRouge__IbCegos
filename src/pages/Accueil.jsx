8f42074d6fe755c15de3ad8bf4e7f3f41905ee8e
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2023-01-19 14:54:01 +0100

    feat: Intégration de la Bannière à la page d'Accueil :tada:

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -1,10 +1,12 @@
 import { Searchbar } from "../communs/Searchbar/Searchbar";
 import { AffichageCartePrestation } from "../composants/AffichageCartes/AffichageCartesPrestation";
+import Banner from "../composants/Banniere/Banniere";
 
 export const Accueil = () => {
   return (
     <>
       <Searchbar />
+      <Banner />
       <AffichageCartePrestation />
     </>
   );
