2cdf3bea84de63eaf1c911b70e665d891d586cff
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2023-01-19 14:49:35 +0100

    style: Changement de nom de classe "container" :lipstick:

--- src/communs/Footer/Footer.jsx
+++ src/communs/Footer/Footer.jsx
@@ -1,24 +1,24 @@
-import './Footer.css'
+import "./Footer.css";
 import { LiensFooter } from "../../composants/LiensFooter/LiensFooter";
 
 export const Footer = () => {
-    return (
-        <footer className="footer">
-            <container className="container">
-                <LiensFooter href="" text="Centre d'aide"/>
-                <LiensFooter href="" text="FAQ"/>
-                <LiensFooter href="" text="Nous contacter"/>
-            </container>
-            <container className="container">
-                <LiensFooter href="" text="UMC"/>
-                <LiensFooter href="" text="Présentation UMC"/>
-                <LiensFooter href="" text="Nos services"/>
-            </container>
-            <container className="container">
-                <LiensFooter href="" text="Informations légales"/>
-                <LiensFooter href="" text="Mentions légales"/>
-                <LiensFooter href="" text="Politique de confidentialité"/>
-            </container>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <container className="containerFooter">
+        <LiensFooter href="" text="Centre d'aide" />
+        <LiensFooter href="" text="FAQ" />
+        <LiensFooter href="" text="Nous contacter" />
+      </container>
+      <container className="containerFooter">
+        <LiensFooter href="" text="UMC" />
+        <LiensFooter href="" text="Présentation UMC" />
+        <LiensFooter href="" text="Nos services" />
+      </container>
+      <container className="containerFooter">
+        <LiensFooter href="" text="Informations légales" />
+        <LiensFooter href="" text="Mentions légales" />
+        <LiensFooter href="" text="Politique de confidentialité" />
+      </container>
+    </footer>
+  );
+};
