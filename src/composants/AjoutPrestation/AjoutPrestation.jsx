694a8edabe5e6c315c59a89f49d73028f79bd258
Sofiane Salhi <sofiane.slhi@gmail.com>
2023-01-20 16:50:26 +0100

    feat: ajout prestation

--- src/composants/AjoutPrestation/AjoutPrestation.jsx
+++ src/composants/AjoutPrestation/AjoutPrestation.jsx
@@ -3,28 +3,28 @@ import "../AjoutPrestation/AjoutPrestation.css";
 const AjoutPrestation = (props) => {
   return (
     <>
-    <h2>NOUVELLE PRESTATION</h2>
-      <label for="nom">nom
-        <input type="text" name="nom" />
+    <h2 className="h2ajoutprestation">NOUVELLE PRESTATION</h2>
+      <label for="nom" className="nomajoutprestation">nom
+        <input className="input1" type="text" name="nom" />
       </label>
 
-      <label for="localisation">localisation
-        <input type="text" name="name" />
+      <label for="localisation"  className="localisationajoutprestation">localisation
+        <input type="input2" name="name" />
       </label>
 
-      <label for="taux horaires" >taux d'horaires
-        <input type="text" name="name" />
+      <label for="taux horaire" className="tauxhoraireajoutprestation" >taux horaire
+        <input type="input1" name="name" />
       </label>
 
-      <label for= "description">description
-        <input type="text" name="name" />
+      <label for= "description" className="descriptionajoutprestation">description
+        <input className= "input3" type="text" name="name" />
       </label>
 
-      <label for="categorie">categorie
-        <input type="text" name="name" />
+      <label for="categorie" className="categorieajoutprestation">categorie
+        <input type="input1" name="name" />
       </label>
 
-      <button>Ajouter une prestation</button>
+      <button className= "boutonajoutprestation" onClick={props.onClick}>Ajouter une prestation{props.text}</button>
 
     </>
   );
