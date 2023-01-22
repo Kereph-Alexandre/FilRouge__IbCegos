e12b3323a99e0031e7566ca3c226bb29190a491b
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-22 14:13:04 +0100

    struct : Changement de fonctionnement de la SearchBar

--- src/communs/Searchbar/Searchbar.jsx
+++ src/communs/Searchbar/Searchbar.jsx
@@ -1,19 +1,36 @@
-import { Bouton } from "../../composants/Bouton/Bouton";
-import { SearchbarItem } from "../../composants/SearchbarItem/SearchbarItem";
 import "./Searchbar.css";
+import { Icon } from "@iconify/react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Searchbar = (props) => {
+  const [motClef, setMotClef] = useState("");
+  const navigation = useNavigate();
+
+  const fonctionRecherche = () => {
+    navigation(`/recherche/${motClef}`);
+  };
+
   return (
-    <span className="containerSearchbar">
-      <div className="bouton">
-        <Bouton text="Trouver une entreprise" />
-      </div>
-      <div className="searchbaritem">
-        <SearchbarItem
-          changementMotClef={props.changementMotClef}
-          placeholder="Recherche"
+    <div className="Searchbar">
+      <div className="searchVisual">
+        <input
+          className="SearchText"
+          type="search"
+          id="searchbaritem"
+          placeholder={props.placeholder}
+          onChange={(evenement) => setMotClef(evenement.target.value)}
         />
+
+        <button className="SearchButton">
+          <Icon
+            className="icon"
+            icon="ph:magnifying-glass-bold"
+            color="white"
+            onClick={fonctionRecherche}
+          />
+        </button>
       </div>
-    </span>
+    </div>
   );
 };
