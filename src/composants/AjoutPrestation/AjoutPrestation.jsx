07bc2bb7b0bb1ae52ff568314efee5ed2fe810c8
Sofiane Salhi <sofiane.slhi@gmail.com>
2023-01-23 16:43:48 +0100

    feat: changement ajout prestation

--- src/composants/AjoutPrestation/AjoutPrestation.jsx
+++ src/composants/AjoutPrestation/AjoutPrestation.jsx
@@ -4,24 +4,25 @@ const AjoutPrestation = (props) => {
   return (
     <>
     <h2 className="h2ajoutprestation">NOUVELLE PRESTATION</h2>
+
       <label for="nom" className="nomajoutprestation">nom
         <input className="input1" type="text" name="nom" />
       </label>
 
       <label for="localisation"  className="localisationajoutprestation">localisation
-        <input type="input2" name="name" />
+        <input className="input1" type="text" name="name" />
       </label>
 
       <label for="taux horaire" className="tauxhoraireajoutprestation" >taux horaire
-        <input type="input1" name="name" />
+        <input className="input1" type="text" name="name" />
       </label>
 
       <label for= "description" className="descriptionajoutprestation">description
-        <input className= "input3" type="text" name="name" />
+        <input className= "input2" type="text" name="name" />
       </label>
 
       <label for="categorie" className="categorieajoutprestation">categorie
-        <input type="input1" name="name" />
+        <input className="input1" type="text" name="name" />
       </label>
 
       <button className= "boutonajoutprestation" onClick={props.onClick}>Ajouter une prestation{props.text}</button>
