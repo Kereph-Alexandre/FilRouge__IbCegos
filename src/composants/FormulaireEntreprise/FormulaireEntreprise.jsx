972ede6fa4dd0794222fa4f9195ceb875e06c7e0
Alexandre Bouriez <alexandre.bouriez@laposte.net>
2023-01-27 16:40:05 +0100

    fix: Ajout de données entreprise et Modification par formulaire :tada:

--- src/composants/FormulaireEntreprise/FormulaireEntreprise.jsx
+++ src/composants/FormulaireEntreprise/FormulaireEntreprise.jsx
@@ -1,25 +1,54 @@
 // import { useParams } from "react-router-dom";
+import { useState } from "react";
 import "./FormulaireEntreprise.css";
 
 export const FormulaireEntreprise = () => {
+  const Entreprise = {
+    id: 1,
+    nom: "Lapeyre",
+    img: "https://dummyimage.com/100x100/000/fff",
+    presentation: "y'en a pas deux !",
+    siret: 12345678901234,
+    effectif: 22,
+    domaineActivite: "Aménagement et ameublement",
+    zoneGeo: "France",
+  };
+  const [presentation, setPresentation] = useState(Entreprise.presentation);
+  const [siret, setSiret] = useState(Entreprise.siret);
+  const [effectif, setEffectif] = useState(Entreprise.effectif);
+  const [domaine, setDomaine] = useState(Entreprise.domaineActivite);
+  const [zoneGeo, setZoneGeo] = useState(Entreprise.zoneGeo);
   //   const { id } = useParams();
 
   //If Id null/undef alors c'est un formulaire de création
   //Else, c'est un formulaire de mise à jour
 
+  const mettreAJour = (event) => {
+    event.preventDefault();
+    Entreprise.domaineActivite = domaine;
+    Entreprise.effectif = effectif;
+    Entreprise.zoneGeo = zoneGeo;
+    Entreprise.siret = siret;
+    Entreprise.presentation = presentation;
+
+    console.log(Entreprise);
+  };
+
   return (
     <main className="espaceEntreprise">
-      <form className="formulaireEntreprise">
+      <form className="formulaireEntreprise" onSubmit={mettreAJour}>
         <div className="imageEntreprise">
-          <img src="https://dummyimage.com/100x100/000/fff" alt="" />
+          <img src={Entreprise.img} alt={Entreprise.nom} />
           <div className="choixImage">
             <h2>Logo de l'entreprise</h2>
-            <label htmlFor="logoEntreprise"></label>
-            <input
-              type="file"
-              placeholder="Ajouter une url d'image"
-              name="logoEntreprise"
-            />
+            <label htmlFor="logoEntreprise">
+              Modifier votre logo
+              <input
+                type="file"
+                placeholder="Ajouter une url d'image"
+                name="logoEntreprise"
+              />
+            </label>
           </div>
         </div>
         <div className="encartDescriptionEntreprise">
@@ -30,6 +59,8 @@ export const FormulaireEntreprise = () => {
             rows={10}
             name="descriptionEntreprise"
             id="descriptionEntreprise"
+            value={presentation}
+            onChange={(event) => setPresentation(event.target.value)}
           />
         </div>
         <div className="informationsEntreprise">
@@ -40,6 +71,8 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="numéro siret"
               name="siretEntreprise"
+              value={siret}
+              onChange={(event) => setSiret(event.target.value)}
             />
           </div>
           <div>
@@ -48,6 +81,8 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="taille de l'effectif"
               name="effectifEntreprise"
+              value={effectif}
+              onChange={(event) => setEffectif(event.target.value)}
             />
           </div>
           <div>
@@ -58,6 +93,8 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="domaine d'activité"
               name="domaineActiviteEntreprise"
+              value={domaine}
+              onChange={(event) => setDomaine(event.target.value)}
             />
           </div>
           <div>
@@ -66,6 +103,8 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="zone d'intervention géographique"
               name="zoneGeoEntreprise"
+              value={zoneGeo}
+              onChange={(event) => setZoneGeo(event.target.value)}
             />
           </div>
         </div>
