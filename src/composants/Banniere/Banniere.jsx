dd995e0d35cf649c656dad3860acd6995fe433b4
Audrey <audrey.grout@gmail.com>
2023-01-27 09:53:59 +0100

    fix: Modification du texte de la bannière :bug:

--- src/composants/Banniere/Banniere.jsx
+++ src/composants/Banniere/Banniere.jsx
@@ -6,17 +6,8 @@ const Banner = () => {
   return (
     <div className="conteneurBanniere">
       <p className="descriptionBanniere">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
-        sed justo sit amet tortor venenatis lacinia et sed nulla. Mauris
-        molestie sagittis pulvinar. Mauris dignissim purus quis orci iaculis
-        varius. Aliquam eu massa in enim viverra cursus ut id leo. Duis sem
-        lacus, scelerisque at turpis et, laoreet vulputate elit. Morbi bibendum
-        arcu id malesuada interdum. Morbi mattis convallis lectus, in auctor
-        nibh feugiat vitae. Fusce sollicitudin leo in nibh luctus fringilla. In
-        erat libero, pharetra ut urna non, suscipit congue velit. Maecenas
-        vestibulum lacus nec tellus volutpat elementum. Cras in est diam.
-        Suspendisse malesuada faucibus risus, sit amet lacinia justo scelerisque
-        et. Nulla facilisi. Donec varius est ut iaculis tincidunt.
+        Besoin d'accompagnement dans le choix de vos matériaux ? De câbler une armoire électrique ? Ou tout simplement repeindre votre salon ? UltraMotionCorp sera le partenaire dans votre recherche de prestations de services. <br />
+        Grandes entreprises ou indépendants mettent à votre disposition leur savoir-faire en matière de bricolage, afin de vous accompagner au mieux dans tous les travaux que vous entreprenez.
       </p>
       <img className="imageBanniere" src={image} alt="illustration" />
     </div>
