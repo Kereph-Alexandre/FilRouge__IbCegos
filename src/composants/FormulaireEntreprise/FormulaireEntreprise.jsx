bb0c774af9c9470783944508f8cd128e74991409
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-01-28 15:09:56 +0100

    fix: finalisation de la page FormulaireEntreprise :tada:

--- src/composants/FormulaireEntreprise/FormulaireEntreprise.jsx
+++ src/composants/FormulaireEntreprise/FormulaireEntreprise.jsx
@@ -1,62 +1,71 @@
 // import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { Alert, Snackbar } from "@mui/material";
+import { useEffect, useState } from "react";
 import "./FormulaireEntreprise.css";
 
 export const FormulaireEntreprise = () => {
-  let Entreprise = {
-    id: 1,
-    nom: "Lapeyre",
-    img: "https://dummyimage.com/100x100/000/fff",
-    presentation: "y'en a pas deux !",
-    siret: 12345678901234,
-    effectif: 22,
-    domaineActivite: "Aménagement et ameublement",
-    zoneGeo: "France",
-  };
-  const [presentation, setPresentation] = useState(Entreprise.presentation);
-  const [siret, setSiret] = useState(Entreprise.siret);
-  const [effectif, setEffectif] = useState(Entreprise.effectif);
-  const [domaine, setDomaine] = useState(Entreprise.domaineActivite);
-  const [zoneGeo, setZoneGeo] = useState(Entreprise.zoneGeo);
+  const [alerte, setAlerte] = useState(false);
+
+  const [entreprise, setEntreprise] = useState({
+    img: "http://dummyimage.com/165x250.png/5fa2dd/ffffff",
+    presentation: "",
+    siret: 0,
+    effectif: 0,
+    domaine: "",
+    zoneGeo: "",
+  });
   //   const { id } = useParams();
 
   //If Id null/undef alors c'est un formulaire de création
   //Else, c'est un formulaire de mise à jour
 
+  useEffect(() => {
+    fetch(`http://localhost:3004/Entreprise/1`).then((response) =>
+      response.json().then((data) => setEntreprise(data))
+    );
+  }, []);
+
   const mettreAJour = (event) => {
     event.preventDefault();
-    Entreprise.domaineActivite = domaine;
-    Entreprise.effectif = effectif;
-    Entreprise.zoneGeo = zoneGeo;
-    Entreprise.siret = siret;
-    Entreprise.presentation = presentation;
 
-    console.log(Entreprise);
+    setEntreprise({
+      ...entreprise,
+      presentation: event.target.descriptionEntreprise.value,
+      siret: event.target.siretEntreprise.value,
+      effectif: event.target.effectifEntreprise.value,
+      domaine: event.target.domaineActiviteEntreprise.value,
+      zoneGeo: event.target.zoneGeoEntreprise.value,
+    });
+
+    fetch(`http://localhost:3004/Entreprise/1`, {
+      method: "PUT",
+      body: JSON.stringify(entreprise),
+      headers: { "content-type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then(setAlerte(true));
   };
 
   return (
     <main className="espaceEntreprise">
       <form className="formulaireEntreprise" onSubmit={mettreAJour}>
         <div className="imageEntreprise">
-          <img src={Entreprise.img} alt={Entreprise.nom} />
+          <img src={entreprise.img} alt={entreprise.nom} />
           <div className="choixImage">
             <h2>Logo de l'entreprise</h2>
-            <label htmlFor="logoEntreprise">
-              Modifier votre logo
+            <div className="imageLabelInput">
+              <label htmlFor="logoEntreprise">
+                Entrez l'url de votre image
+              </label>
               <input
-                type="file"
-                placeholder="Ajouter une url d'image"
+                type="url"
                 name="logoEntreprise"
+                value={entreprise.img}
+                onChange={(event) =>
+                  setEntreprise({ ...entreprise, img: event.target.value })
+                }
               />
-            </label>
-          </div>
-          <div className="resumeEntreprise">
-            <p>Nom : {Entreprise.nom}</p>
-            <p>Présentation : {Entreprise.presentation}</p>
-            <p>Siret : {Entreprise.siret}</p>
-            <p>Effectif : {Entreprise.effectif}</p>
-            <p>Domaine D'activité : {Entreprise.domaineActivite}</p>
-            <p>Zone Géographique : {Entreprise.zoneGeo}</p>
+            </div>
           </div>
         </div>
         <div className="encartDescriptionEntreprise">
@@ -64,11 +73,13 @@ export const FormulaireEntreprise = () => {
           <label htmlFor="descriptionEntreprise"></label>
           <textarea
             cols={30}
-            rows={10}
+            rows={5}
             name="descriptionEntreprise"
             id="descriptionEntreprise"
-            value={presentation}
-            onChange={(event) => setPresentation(event.target.value)}
+            value={entreprise.presentation}
+            onChange={(event) =>
+              setEntreprise({ ...entreprise, presentation: event.target.value })
+            }
           />
         </div>
         <div className="informationsEntreprise">
@@ -79,8 +90,10 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="numéro siret"
               name="siretEntreprise"
-              value={siret}
-              onChange={(event) => setSiret(event.target.value)}
+              value={entreprise.siret}
+              onChange={(event) =>
+                setEntreprise({ ...entreprise, siret: event.target.value })
+              }
             />
           </div>
           <div>
@@ -89,8 +102,10 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="taille de l'effectif"
               name="effectifEntreprise"
-              value={effectif}
-              onChange={(event) => setEffectif(event.target.value)}
+              value={entreprise.effectif}
+              onChange={(event) =>
+                setEntreprise({ ...entreprise, effectif: event.target.value })
+              }
             />
           </div>
           <div>
@@ -101,8 +116,13 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="domaine d'activité"
               name="domaineActiviteEntreprise"
-              value={domaine}
-              onChange={(event) => setDomaine(event.target.value)}
+              value={entreprise.domaineActivite}
+              onChange={(event) =>
+                setEntreprise({
+                  ...entreprise,
+                  domaineActivite: event.target.value,
+                })
+              }
             />
           </div>
           <div>
@@ -111,14 +131,21 @@ export const FormulaireEntreprise = () => {
               type="text"
               placeholder="zone d'intervention géographique"
               name="zoneGeoEntreprise"
-              value={zoneGeo}
-              onChange={(event) => setZoneGeo(event.target.value)}
+              value={entreprise.zoneGeo}
+              onChange={(event) =>
+                setEntreprise({ ...entreprise, zoneGeo: event.target.value })
+              }
             />
           </div>
         </div>
-        <div className="alacon">
+        <div className="boutonConfirmer">
           <button type="submit">Modifier</button>
         </div>
+        <Snackbar open={alerte} onClose={() => setAlerte(false)}>
+          <Alert severity="success" sx={{ width: "100%" }}>
+            Modifications effectuées
+          </Alert>
+        </Snackbar>
       </form>
     </main>
   );
