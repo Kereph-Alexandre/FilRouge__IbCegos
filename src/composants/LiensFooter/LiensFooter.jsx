60fdf08aacdaee1cbd52a02f73233187b66cd153
Kereph-Alexandre <114568855+Kereph-Alexandre@users.noreply.github.com>
2023-02-09 15:14:26 +0100

    remplacement de fichier LiensFooter perdu

---
+++ src/composants/LiensFooter/LiensFooter.jsx
@@ -0,0 +1,14 @@
+import "./LiensFooter.css";
+
+/**
+ * Pour afficher les liens dans le footer
+ * @param {{href:string, text:string}} props
+ * @returns un lien
+ */
+export const LiensFooter = (props) => {
+  return (
+    <a className="liensFooter" href={props.href}>
+      {props.text}
+    </a>
+  );
+};
